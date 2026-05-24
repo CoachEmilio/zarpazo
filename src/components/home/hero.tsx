b00c08eec2e5ddecef69b4725fd9dca7b5e73558
Coach-Emilio <escueladelsurdubai@gmail.com>
2026-05-24 00:10:45 -0300

    fix her space height

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,16 +5,16 @@ export default function Hero() {
   return (
     <section
       aria-labelledby="hero-title"
-      className="relative w-full overflow-hidden bg-black px-6 pb-8 pt-6 md:pb-12 md:pt-8"
+      className="relative w-full overflow-hidden bg-black px-6 pb-4 pt-3 md:pb-12 md:pt-8"
     >
-      <div className="mx-auto flex min-h-[58svh] max-w-5xl flex-col items-center justify-start text-center">
-        <div className="relative mb-6 h-48 w-48 max-w-full md:h-64 md:w-64 lg:h-72 lg:w-72">
+      <div className="mx-auto flex min-h-[28svh] md:min-h-[58svh] max-w-5xl flex-col items-center justify-start text-center">
+        <div className="relative mb-4 h-36 w-36 max-w-full md:h-64 md:w-64 lg:h-72 lg:w-72">
           <Image
             src={config.brand.logoPath}
             alt={config.brand.name}
             fill
             priority
-            sizes="(max-width: 768px) 192px, (max-width: 1024px) 256px, 288px"
+            sizes="(max-width: 768px) 144px, (max-width: 1024px) 256px, 288px"
             className="object-contain"
           />
         </div>
