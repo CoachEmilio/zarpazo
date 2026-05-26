45a09c021367b497816d8cbeaa197c032a680dfe
Coach-Emilio <escueladelsurdubai@gmail.com>
2026-05-26 14:02:18 -0300

    fixing whatsapp business number

--- src/data/config.ts
+++ src/data/config.ts
@@ -5,7 +5,7 @@ export const config = {
     siteUrl: "https://www.zarpazo.art",
     logoPath: "/brand/zarpazo-logo.png",
   },
-  whatsapp: "54911737346352",
+  whatsapp: "5491173746352",
   correo: "infozarpazo@gmail.com",
   instagram: "https://instagram.com/zarpazo.art"
 }
